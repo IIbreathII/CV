4c4e91fe5c6668d87d253efe6f8ce4b07e51b687
Artem <artemstarik7@gmail.com>
2025-09-01 11:37:17 +0300

    landing+navigation

--- front/components/home/landing/page.tsx
+++ front/components/home/landing/page.tsx
@@ -1,18 +1,36 @@
 "use client"
 
 import style from "./styles/landing.module.css"
+import Image from 'next/image';
+
 
 export default function LandingBlock() {
     return (
         <div className={style.landing_wrapper}>
-            <div className={style.landing_background}>
-                <h1 className={style.title}>
-                    I help travelers start their
-                    <span>
-                    own journey into 
-                    </span>
-                    startups and business
-                </h1>
+            <div className={style.back_1}>
+                <div className={style.landing_background}>
+                    <h1 className={style.title}>
+                        Lorem self Ipsum
+                        <span>
+                        own Lorem Ipsum
+                        </span>
+                        Lorem Ipsum and Lorem Ipsum
+                    </h1>
+                    <div role="navigation" aria-label="Навигация по разделам" className={style.navigation}>
+                        <button className={style.contact_button}>
+                        let’s contact
+                        <a href="">let’s contact </a>
+                        <Image
+                            src="/images/landing/arrow.png"
+                            alt="стрелка"
+                            width={5}
+                            height={5}
+                            className={style.arrow}
+                        />
+                        </button>
+                        <button className={style.navigation_button}>more</button>
+                    </div>
+                </div>           
             </div>
         </div>
     );
--- front/components/home/navigation/page.tsx
+++ front/components/home/navigation/page.tsx
@@ -1,10 +1,22 @@
 "use client"
-
+import style from "./styles/navigation.module.css"
 
 export default function NavigationBlock() {
     return (
-        <div className="">
-         
+        <div className={style.navigation_wrapper}>
+            <div className={style.navigation_block}>
+                <div className={style.top_area_left} aria-hidden="true"></div>
+                <div className={style.top_area_right} aria-hidden="true"></div>
+                    <div className={style.navigation_content}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    </div>
+                <div className={style.bot_area_left} aria-hidden="true"></div>
+                <div className={style.bot_area_right} aria-hidden="true"></div>
+            </div>
         </div>
     );
-}
+} 
